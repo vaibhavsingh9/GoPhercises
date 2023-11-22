e1ddb324bc1333ec0713cd96238bcfe40a373a63
vaibhavsingh9 <vaibhav0001singh@gmail.com>
2023-11-22 23:38:01 +0530

    final commit

--- adventureTime/story.go
+++ adventureTime/story.go
@@ -1,8 +1,12 @@
 package adventureTime
 
 import (
+	"encoding/json"
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func init() {
@@ -85,10 +89,64 @@ func WithTemplate(t *template.Template) HandlerOption {
 }
 
 func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
-
+	return func(h *handler) {
+		h.pathFn = fn
+	}
 }
+
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
+}
+
+type handler struct {
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+func defaultPathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
+
+	if chapter, ok := h.s[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "chapter not found.", http.StatusNotFound)
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	d := json.NewDecoder(r)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+type Story map[string]Chapter
 
 type Chapter struct {
 	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
+}
+
+type Option struct {
+	text    string `json:"text"`
+	Chapter string `json:"arc"`
 }
